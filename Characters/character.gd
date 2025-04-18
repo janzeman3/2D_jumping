ea9aabfeace3849781bd483476a23faa3cfa2dba
Jan Zeman <major.su@seznam.cz>
2025-04-18 16:11:51 +0200

    Map + Ninja Frog
    Signed-off-by: Jan Zeman <major.su@seznam.cz>

---
+++ Characters/character.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+class_name character
+
+# rychlostp ohybu doleva/doprava
+var speed = 150
+var gravitation = 10
+var jump = 400
+
+func _physics_process(_delta: float) -> void:
+	# jumping and falling (vertical move)
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = - jump
+	elif not is_on_floor():
+		velocity.y += gravitation
+
+	if velocity.y < 0:
+		$AnimatedSprite2D.play("jump")
+	else:
+		$AnimatedSprite2D.play("fall")
+
+	# runing or horizontal flying (horizontal move)
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * speed
+		if is_on_floor():
+			$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = (direction<0)
+	else:
+		velocity.x = 0
+		if is_on_floor():
+			$AnimatedSprite2D.play("idle")
+ 
+	move_and_slide()
