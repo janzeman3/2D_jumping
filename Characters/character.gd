dcedf35e5446866fe362dfd175db70511c7ab10d
Jan Zeman <major.su@seznam.cz>
2025-04-18 16:24:50 +0200

    Mask Dude
    Signed-off-by: Jan Zeman <major.su@seznam.cz>

--- Characters/character.gd
+++ Characters/character.gd
@@ -11,13 +11,13 @@ func _physics_process(_delta: float) -> void:
 	# jumping and falling (vertical move)
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = - jump
+		$AnimatedSprite2D.play("jump")
 	elif not is_on_floor():
 		velocity.y += gravitation
-
-	if velocity.y < 0:
-		$AnimatedSprite2D.play("jump")
-	else:
-		$AnimatedSprite2D.play("fall")
+		if velocity.y < 0:
+			$AnimatedSprite2D.play("jump")
+		else:
+			$AnimatedSprite2D.play("fall")
 
 	# runing or horizontal flying (horizontal move)
 	var direction = Input.get_axis("left", "right")
