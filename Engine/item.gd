853f13dd14f374f3f8bf529a8602e7f4b55124a7
Jan Zeman <major.su@seznam.cz>
2025-04-24 15:44:38 +0200

    End-game detection
    Game over is when all fruit is eaten.
    
    Signed-off-by: Jan Zeman <major.su@seznam.cz>

--- Engine/item.gd
+++ Engine/item.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+class_name Fruit
+
 func _on_body_entered(body: Node2D) -> void:
 	if body is Character:
 		var score = get_meta("Score")
--- main_screen.gd
+++ main_screen.gd
@@ -2,7 +2,28 @@ extends Node2D
 
 func _ready() -> void:
 	Game.scoreAdded.connect(on_addScore)
+	gameStart()
 
 func on_addScore():
 	$ScoreCounter.text = "%d" % Game.score
+
+func _on_end_check_timeout() -> void:
+	var child_number = get_child_count()
+	var idx = 0
+	var found_fruit = false
 	
+	while not found_fruit and idx < child_number:
+		if get_child(idx) is Fruit:
+			found_fruit = true
+			print("EndChecker:" + get_child(idx).name + " found")
+		idx += 1
+	
+	if not found_fruit:
+		print("EndChecker: Game Over!")
+		gameOver()
+
+func gameStart():
+	$EndCheck.start()
+
+func gameOver():
+	$EndCheck.stop()
